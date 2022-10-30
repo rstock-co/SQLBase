fd6655198b3f38a4502e80a26a6e4ac1baef068e
Richard Stock <rstock.co@gmail.com>
2022-10-30 16:52:56 -0600

    Fix imports on Home component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,44 +1,37 @@
 import "./Home.scss";
-import Paper from '@mui/material/Paper'
-import Container from '@mui/material/Container';
-import Banner from '../styles/banner/Banner'
-import ProductFeature from "../styles/product-feature/ProductFeature";
-import TargetUsers from "../styles/product-feature/TargetUsers";
-import TestimonialCarousel from "../styles/product-feature/TestimonialCarousel";
-import AboutUs from "../styles/aboutus/AboutUs";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "../styles/theme/theme.js";
-import PageSplitter from "../styles/components/PageSplitter";
-
-
-
+import theme from "../../styles/theme/theme.js";
+import Banner from "../../styles/banner/Banner";
+import ProductFeature from "../../styles/product-feature/ProductFeature";
+import TargetUsers from "../../styles/product-feature/TargetUsers";
+import TestimonialCarousel from "../../styles/product-feature/TestimonialCarousel";
+import AboutUs from "../../styles/aboutus/AboutUs";
+import PageSplitter from "../../styles/components/PageSplitter";
 
 const Home = () => {
   return (
     <ThemeProvider theme={theme}>
-
-      <Container maxWidth='false'>
-
+      <Container maxWidth="false">
         <Paper elevation={12} className="landing-paper">
           <Banner />
-          <PageSplitter className="page-splitter" src="banner-body.jpeg" alt="banner-split" />
-          <Container maxWidth='md' sx={{ p: 6, }} >
+          <PageSplitter
+            className="page-splitter"
+            src="banner-body.jpeg"
+            alt="banner-split"
+          />
+          <Container maxWidth="md" sx={{ p: 6 }}>
             <ProductFeature />
             <br />
             <TargetUsers />
             <TestimonialCarousel />
             <AboutUs />
-
-
-
-
-
           </Container>
         </Paper>
       </Container>
     </ThemeProvider>
-
-  )
+  );
 };
 
 export default Home;
