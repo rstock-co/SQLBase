6d56e122b1d82196e528d2d163138df31dbac4b2
Richard Stock <rstock.co@gmail.com>
2022-10-30 15:44:13 -0600

    Update imports for SchemaTable component

--- client/src/components/tables/SchemaTable.js
+++ client/src/components/tables/SchemaTable.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 const SchemaTable = ({ table, fields }) => {
   return (
