aad6bca850202a79c1b3255c3f547513074ce18f
Richard Stock <rstock.co@gmail.com>
2022-10-30 16:52:07 -0600

    Refactor Application component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,37 @@
-import "./App.css";
-import useApplicationData from "./hooks/useApplicationData";
-import { ThemeProvider } from "@mui/system";
-import { Button, Container } from '@mui/material';
-import { useEffect } from "react";
-import Banner from "./styles/banner.js/banner";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import Layout from "./components/routes/Layout";
+import Home from "./components/pages/Home";
+import CreateTablesPage from "./components/pages/CreateTables";
+import CreateQueriesPage from "./components/pages/CreateQueries";
 
-
-const App = () => {
-  const { state, dispatch } = useApplicationData();
-  const userList = state.users.map(user => (
-    <li key={user.id}>
-      {" "}
-      {user.first_name} {user.last_name} {user.email}{" "}
-    </li>
-  ));
+export default function App() {
   return (
-    // <div className="App">
-    //   <h1> Users </h1>
-
-    //   <ul> {userList} </ul>
-    // </div>
-    <>
-      <Banner />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="tables" element={<CreateTablesPage />} />
+          <Route path="queries" element={<CreateQueriesPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
-export default App;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
