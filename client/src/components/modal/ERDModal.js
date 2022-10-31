f05659ba4a786b2463b69431d73d081d7fb6796f
Travis <travisliu708@gmail.com>
2022-10-31 15:36:04 -0700

    add mermaid to modal

--- client/src/components/modal/ERDModal.js
+++ client/src/components/modal/ERDModal.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Modal, Box, Typography, Button } from '@mui/material';
+import mm from 'mermaid'
+import Mermaid from './Mermaid';
 
 
 const style = {
@@ -7,12 +9,67 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
+  width: '85%',
+  height: '85%',
+  bgcolor: '#21222c',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
+
+const example = `
+classDiagram
+class GeoPointType {
+ <<enumeration>>
+  BROWNFIELD
+  OGWELL
+  CELL_TOWER
+  NUCLEAR_REACTOR
+  SUPERFUND
+}
+class GeoPoint {
+  -UUID id
+  +GeoPointType type
+  +GeographyPoint location
+  -UUID metadata references metadata(id)
+  +Datetime createdAt
+}
+class GeographyPoint {
+  <<interface>>
+  +GeoJSON geojson
+  +Int srid
+  +Float longitude
+  +Float latitude
+}
+class NearbyPoint {
+ <<Interface>>
+  -UUID id references GeoPoint(id)
+  +GeoPointType GeoPoint::type
+  +GeographyPoint GeoPoint::location
+  +UUID GeoPoint::metadata
+  +Float distance
+}
+class NearbyPoints {
+<<Service>>
+  +GeoJSON origin
+  +Float radiusMi
+  +Int first
+  +Int last
+  +Int offset
+  +Cursor before
+  +Cursor after
+}
+class Hotel {
+ -UUID id
++String name
+-Int objectid 
+}
+GeoPoint *-- GeoPointType: Composition
+GeoPoint *-- GeographyPoint: Composition
+GeoPoint "1" <|-- "1" NearbyPoint: Implements
+NearbyPoints "1" -- "0..n"NearbyPoint: Contains
+Hotel "1" -- "1" GeoPoint: May Contain
+`
 
 const ERDModal = (state, open) => {
   return (
@@ -20,14 +77,11 @@ const ERDModal = (state, open) => {
       open={open}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
+      sx={{
+      }}
     >
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+        <Mermaid chart={example} />
       </Box>
     </Modal>
   );
