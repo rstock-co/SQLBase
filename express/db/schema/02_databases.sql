85fd6da861c7c8248684cbe6e6e728a189567616
Richard Stock <rstock.co@gmail.com>
2022-10-30 20:39:59 -0600

    Minor fixes

--- express/db/schema/02_databases.sql
+++ express/db/schema/02_databases.sql
@@ -4,25 +4,22 @@ DROP TABLE IF EXISTS query_forms CASCADE;
 CREATE TABLE databases(
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  name VARCHAR(255), 
+  name VARCHAR(255),
   schema_string TEXT,
   query_string TEXT,
   seed_string TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT NULL
 );
-
 CREATE TABLE schema_forms(
   id SERIAL PRIMARY KEY NOT NULL,
   database_id INTEGER REFERENCES databases(id) ON DELETE CASCADE,
   schema_form_json json,
   form_schema_string TEXT
 );
-
 CREATE TABLE query_forms(
   id SERIAL PRIMARY KEY NOT NULL,
   database_id INTEGER REFERENCES databases(id) ON DELETE CASCADE,
   query_form_json json,
   form_schema_string TEXT
 );
-
