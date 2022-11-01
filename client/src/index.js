f8289fa65bfeb952fa9f512d4d183e97bdb92dfb
Richard Stock <rstock.co@gmail.com>
2022-11-01 16:28:00 -0600

    Add Global Provider context to App (wrap routes)

--- client/src/index.js
+++ client/src/index.js
@@ -2,36 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-// import App from './App';
-import reportWebVitals from "./reportWebVitals";
 import Layout from "./components/routes/Layout";
 import Home from "./components/pages/Home";
 import CreateTablesPage from "./components/pages/CreateTables";
 import CreateQueriesPage from "./components/pages/CreateQueries";
+import GlobalProvider from "./state/GlobalStateProvider";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="tables" element={<CreateTablesPage />} />
-          <Route path="queries" element={<CreateQueriesPage />} />
-        </Route>
-      </Routes>
+      <GlobalProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="tables" element={<CreateTablesPage />} />
+            <Route path="queries" element={<CreateQueriesPage />} />
+          </Route>
+        </Routes>
+      </GlobalProvider>
     </BrowserRouter>
   );
 }
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
