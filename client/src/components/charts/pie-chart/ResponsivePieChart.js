67a0ee7172500767f0267715d6a2d400c8719dc5
Richard Stock <rstock.co@gmail.com>
2022-11-05 11:40:50 -0600

    Create Pie Chart main component

---
+++ client/src/components/charts/pie-chart/ResponsivePieChart.js
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from "react";
+import { PieChart, Pie } from "recharts";
+import ActivePieShape from "./ActivePieShape";
+import { pieChartData } from "../../../state/data_structures/chartState";
+
+const ResponsivePieChart = ({
+  width,
+  height,
+  chartColor,
+  textColor1,
+  textColor2,
+}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = useCallback(
+    (_, index) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+
+  return (
+    <PieChart width={width} height={height}>
+      <Pie
+        activeIndex={activeIndex}
+        activeShape={
+          <ActivePieShape textColor1={textColor1} textColor2={textColor2} />
+        }
+        data={pieChartData}
+        cx={width / 2}
+        cy={height / 2}
+        innerRadius={60}
+        outerRadius={80}
+        fill={chartColor}
+        dataKey="value"
+        onMouseEnter={onPieEnter}
+      />
+    </PieChart>
+  );
+};
+export default ResponsivePieChart;
