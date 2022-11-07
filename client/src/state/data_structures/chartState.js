14b0c3ca266a4d7d89bd9d0e67ee17bd5df7c46d
Richard Stock <rstock.co@gmail.com>
2022-11-06 20:42:16 -0700

    Reshape Chart data structure

--- client/src/state/data_structures/chartState.js
+++ client/src/state/data_structures/chartState.js
@@ -1,10 +1,28 @@
-export const pieChartData = [
-  { name: "Ages 20-30", value: 400 },
-  { name: "Ages 30-40", value: 300 },
-  { name: "Ages 40-50", value: 300 },
-  { name: "Ages 50-60", value: 200 },
-  { name: "Ages 60-70", value: 100 },
-];
+export const pieChartData = {
+  age: [
+    { name: "Ages 20-30", value: 10, low: 20, hi: 29 },
+    { name: "Ages 30-40", value: 5, low: 30, hi: 40 },
+    { name: "Ages 40-50", value: 20, low: 40, hi: 49 },
+    { name: "Ages 50-60", value: 15, low: 50, hi: 59 },
+    { name: "Ages 60-70", value: 30, low: 60, hi: 70 },
+  ],
+  salary: [
+    { name: "$30-75k", value: 40, low: 30000, hi: 74999 },
+    { name: "$75-125k", value: 30, low: 75000, hi: 124999 },
+    { name: "$125-175k", value: 15, low: 125000, hi: 174999 },
+    { name: "$175-250k", value: 20, low: 175000, hi: 249999 },
+    { name: "$250-350k", value: 10, low: 250000, hi: 349999 },
+    { name: "$350k +", value: 5, low: 350000, hi: 20000000 },
+  ],
+  years_exp: [
+    { name: "0-5 yrs exp", value: 10, low: 0, hi: 4 },
+    { name: "5-10 yrs exp", value: 5, low: 5, hi: 9 },
+    { name: "10-15 yrs exp", value: 20, low: 10, hi: 14 },
+    { name: "15-20 yrs exp", value: 15, low: 15, hi: 19 },
+    { name: "20-25 yrs exp", value: 30, low: 20, hi: 24 },
+    { name: "25+ yrs exp", value: 30, low: 25, hi: 100 },
+  ],
+};
 
 export const pieChartColors = [
   "#0088FE",
